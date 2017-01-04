135f56dd1147460bddb2a46f8463db1654c1b4ea
Torey Hickman <torey@toreyhickman.com>
2017-01-04 15:40:06 -0500

    Corrects JS validation warnings

--- js/application.js
+++ js/application.js
@@ -1,52 +1,49 @@
-var askedQuestions = []
-var responsesForEmptyQuestion = ["Go on. I'm listening.", "Don't be shy. Let's hear it.", "How can I help you?", "Did you want to ask something?", "Something on your mind?", "Well ... ?"]
-var responsesForStatement = ["Is that a question?", "I can only answer questions."]
-var responsesForRepeatQuestions = ["I already told you.", "You're asking that again?", "I've given you my answer.", "No repeat questions."]
-var responses = ["How would I know?", "Aah, flip a coin.", "That's up to you.", "Yes, no ... maybe?", "I have no earthly idea.", "Signs point ... somewhere.", "Yeah, that's a tough one.", "My sources are unavailable.", "You don't want my advice on that."]
+var askedQuestions = [];
+var responsesForEmptyQuestion = ["Go on. I'm listening.", "Don't be shy. Let's hear it.", "How can I help you?", "Did you want to ask something?", "Something on your mind?", "Well ... ?"];
+var responsesForStatement = ["Is that a question?", "I can only answer questions."];
+var responsesForRepeatQuestions = ["I already told you.", "You're asking that again?", "I've given you my answer.", "No repeat questions."];
+var responses = ["How would I know?", "Aah, flip a coin.", "That's up to you.", "Yes, no ... maybe?", "I have no earthly idea.", "Signs point ... somewhere.", "Yeah, that's a tough one.", "My sources are unavailable.", "You don't want my advice on that."];
 
-var alreadyAsked = function(question, questions) {
-  return questions.include(question) //
-}
 
 $(document).ready(function() {
   $("#ask-button").on("click", function() {
     var question = $("#question").val().trim();
+    var response;
 
     if(question === "") {
       // Get a random item from responsesForEmptyQuestion
-      var response = responsesForEmptyQuestion[Math.floor(Math.random() * responsesForEmptyQuestion.length)];
+      response = responsesForEmptyQuestion[Math.floor(Math.random() * responsesForEmptyQuestion.length)];
 
       $("#answer").fadeOut(600, function() {
         setTimeout(function() {
           $("#answer").text(response).fadeIn(600);
-        }, 400)
+        }, 400);
       });
     } else if(!(/\?$/.test(question))) { // Use RegExp.prototype.test() to check for a question mark
-      var response = responsesForStatement[Math.floor(Math.random() * responsesForStatement.length)];
+      response = responsesForStatement[Math.floor(Math.random() * responsesForStatement.length)];
 
       $("#answer").fadeOut(600, function() {
         setTimeout(function() {
           $("#answer").text(response).fadeIn(600);
-        }, 400)
+        }, 400);
       });
     } else if(askedQuestions.includes(question)) {
-      var response = responsesForRepeatQuestions[Math.floor(Math.random() * responsesForRepeatQuestions.length)];
+      response = responsesForRepeatQuestions[Math.floor(Math.random() * responsesForRepeatQuestions.length)];
 
       $("#answer").fadeOut(600, function() {
         setTimeout(function() {
           $("#answer").text(response).fadeIn(600);
-        }, 400)
+        }, 400);
       });
     } else {
       askedQuestions.push(question);
-      var response = responses[Math.floor(Math.random() * responses.length)];
+      response = responses[Math.floor(Math.random() * responses.length)];
 
       $("#answer").fadeOut(600, function() {
         setTimeout(function() {
           $("#answer").text(response).fadeIn(600);
-        }, 400)
+        }, 400);
       });
-
     }
   });
 });
