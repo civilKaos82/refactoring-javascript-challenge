2a662e2cba996169723d950713ed80f4f905ba6b
Torey Hickman <torey@toreyhickman.com>
2017-01-04 13:07:41 -0500

    Responds to statements

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 var responsesForEmptyQuestion = ["Go on. I'm listening.", "Don't be shy. Let's hear it.", "How can I help you?", "Did you want to ask something?", "Something on your mind?", "Well ... ?"]
+var responsesForStatement     = ["Is that a question?", "I can only answer questions."]
 
 $(document).ready(function() {
   $("#ask-button").on("click", function() {
@@ -7,6 +8,14 @@ $(document).ready(function() {
     if(question === "") {
       // Get a random item from responsesForEmptyQuestion
       var response = responsesForEmptyQuestion[Math.floor(Math.random() * responsesForEmptyQuestion.length)];
+
+      $("#answer").fadeOut(600, function() {
+        setTimeout(function() {
+          $("#answer").text(response).fadeIn(600);
+        }, 400)
+      });
+    } else if(!(/\?$/.test(question))) { // Use RegExp.prototype.test() to check for a question mark
+      var response = responsesForStatement[Math.floor(Math.random() * responsesForStatement.length)];
 
       $("#answer").fadeOut(600, function() {
         setTimeout(function() {
