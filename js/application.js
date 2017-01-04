60bceb999bbab32a4d6b5b4f342b28521e7d34e1
Torey Hickman <torey@toreyhickman.com>
2017-01-04 13:19:58 -0500

    Responds to questions

--- js/application.js
+++ js/application.js
@@ -1,5 +1,6 @@
 var responsesForEmptyQuestion = ["Go on. I'm listening.", "Don't be shy. Let's hear it.", "How can I help you?", "Did you want to ask something?", "Something on your mind?", "Well ... ?"]
-var responsesForStatement     = ["Is that a question?", "I can only answer questions."]
+var responsesForStatement = ["Is that a question?", "I can only answer questions."]
+var responses = ["How would I know?", "Aah, flip a coin.", "That's up to you.", "Yes, no ... maybe?", "I have no earthly idea.", "Signs point ... somewhere.", "Yeah, that's a tough one.", "My sources are unavailable.", "You don't want my advice on that."]
 
 $(document).ready(function() {
   $("#ask-button").on("click", function() {
@@ -22,6 +23,15 @@ $(document).ready(function() {
           $("#answer").text(response).fadeIn(600);
         }, 400)
       });
+    } else {
+      var response = responses[Math.floor(Math.random() * responses.length)];
+
+      $("#answer").fadeOut(600, function() {
+        setTimeout(function() {
+          $("#answer").text(response).fadeIn(600);
+        }, 400)
+      });
+
     }
   });
 });
