c363fd8ffbddf75fd58752263dce4e5b0dc32900
Torey Hickman <torey@toreyhickman.com>
2017-01-04 12:52:23 -0500

    Responds to empty questions

--- js/application.js
+++ js/application.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+var responsesForEmptyQuestion = ["Go on. I'm listening.", "Don't be shy. Let's hear it.", "How can I help you?", "Did you want to ask something?", "Something on your mind?", "Well ... ?"]
 
+$(document).ready(function() {
+  $("#ask-button").on("click", function() {
+    var question = $("#question").val();
+
+    if(question === "") {
+      // Get a random item from responsesForEmptyQuestion
+      var response = responsesForEmptyQuestion[Math.floor(Math.random() * responsesForEmptyQuestion.length)];
+
+      $("#answer").fadeOut(600, function() {
+        setTimeout(function() {
+          $("#answer").text(response).fadeIn(600);
+        }, 400)
+      });
+    }
+  });
 });
